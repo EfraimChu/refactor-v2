d1802168e2552267a2964bade22991205217d9bd
Yunfei.Zhu <yunfei.zhu@shopee.com>
2022-01-21 20:31:11 +0800

    v1

---
+++ demo.js
@@ -0,0 +1,77 @@
+var players = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+};
+
+var invoices = {
+    "customer": "BigCo",
+    "performances": [
+        {"playID": "hamlet", "audience": 55},
+        {"playID": "as-like", "audience": 35},
+        {"playID": "othello", "audience": 40}]
+};
+
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format;
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type)
+            volumeCredits += Math.floor(perf.audience / 5);
+
+        // print line for this order
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+var ret = statement(invoices, players);
+console.log(ret);
+
+//output:
+//Statement for BigCo
+//   Hamlet: $650.00 (55 seats)
+//   As You Like It: $580.00 (35 seats)
+//   Othello: $500.00 (40 seats)
+// Amount owed is $1,730.00
+// You earned 47 credits
+
+
+// 问题：
+//1. 首先，他们希望以HTML格式输出详单。怎么支持？ c-v操作？
+//2. 增加新的表演类型： 如：历史具、田园居， 等等； 增加case ？,
+//3. 如何应对分类规则和计费规则的变化？
+
