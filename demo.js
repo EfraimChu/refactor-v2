4f5d9da1f768be4404b93c7312a1c16220f5c38b
yunfei.zhu <yunfei.zhu@shopee.com>
2022-01-22 18:12:21 +0800

    feat2.8.3 提取函数

--- demo.js
+++ demo.js
@@ -58,6 +58,14 @@ function usd(aNumber) {
         }).format(aNumber);
 }
 
+function totalVolumeCredits(invoice) {
+    let volumeCredits = 0;
+    for (let aPerformance of invoice.performances) {
+        volumeCredits += volumeCreditsFor(aPerformance);
+    }
+    return volumeCredits;
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -68,10 +76,7 @@ function statement(invoice, plays) {
         result += ` ${playFor(aPerformance).name}: ${usd(amount_for(aPerformance) / 100)} (${aPerformance.audience} seats)\n`;
         totalAmount += amount_for(aPerformance);
     }
-    let volumeCredits = 0;
-    for (let aPerformance of invoice.performances) {
-        volumeCredits += volumeCreditsFor(aPerformance);
-    }
+    let volumeCredits = totalVolumeCredits(invoice);
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
